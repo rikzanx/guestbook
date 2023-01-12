526b6472d1da22ceaa953bcb2a1d586806300c6a
rikzanx <erik.kasbam@gmail.com>
2023-01-12 09:26:23 +0700

    add menu sim b

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -90,6 +90,14 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="{{ route('admin.simb.index') }}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Daftar SIM B
+              </p>
+            </a>
+          </li>
           {{-- <li class="nav-item">
             <a href="{{ route('blokir.index') }}" class="nav-link">
               <i class="nav-icon fas fa-building"></i>
